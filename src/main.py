6e97a3770baf6605c12d22971593f32fd724fc78
Andrey-smol <andreyksmol@yandex.ru>
2026-04-27 09:59:50 +0300

    correction find char into sequence

--- src/main.py
+++ src/main.py
@@ -1,28 +1,39 @@
 from src.stack import Stack
 
 
-def checking_balance_brackets(str_: str) -> str:
+def checking_balance_brackets(sequence: str) -> str:
     """
     checking the balance of the brackets
-    :param str_: a string with parentheses
+    :param sequence: a string with parentheses
     :return: сообщение: «Сбалансированно», если строка корректная,
             и «Несбалансированно», если строка составлена неверно.
     """
     stack = Stack()
-    brackets_dict = {
-        'opening': ('(', '[', '{'),
-        'closing': (')', ']', '}')
-    }
-    for letter in str_:
-        if letter in brackets_dict['opening']:
-            stack.push(letter)
-        elif letter in brackets_dict['closing']:
-            idx = brackets_dict['closing'].index(letter)
-            opening_letter = brackets_dict['opening'][idx]
-            if stack.size() and stack.peek() == opening_letter:
-                stack.pop()
-            else:
+    # brackets_dict = {
+    #     'opening': ('(', '[', '{'),
+    #     'closing': (')', ']', '}')
+    # }
+    # for letter in sequence:
+    #     if letter in brackets_dict['opening']:
+    #         stack.push(letter)
+    #     elif letter in brackets_dict['closing']:
+    #         idx = brackets_dict['closing'].index(letter)
+    #         opening_letter = brackets_dict['opening'][idx]
+    #         if stack.size() and stack.peek() == opening_letter:
+    #             stack.pop()
+    #         else:
+    #             return 'Несбалансированно'
+    pairs = {')': '(',
+             ']': '[',
+             '}': '{'}
+    for char in sequence:
+        if char in pairs.values():
+            stack.push(char)
+        elif char in pairs:
+            if stack.is_empty() or stack.peek() != pairs[char]:
                 return 'Несбалансированно'
+            stack.pop()
+
     return 'Сбалансированно' if stack.is_empty() else 'Несбалансированно'
 
 
